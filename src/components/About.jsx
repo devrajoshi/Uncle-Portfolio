0b0b52865744a9c09d82bacd3af82b596de66ca4
Devraj Joshi <154751316+devrajoshi@users.noreply.github.com>
2024-10-31 00:56:00 +0545

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,9 +18,6 @@ const About = () => {
         <div className="w-full md:w-3/4 lg:w-2/3 shadow-lg">
           <p className="text-gray-300 text-center leading-relaxed">
             I am a faculty member at Mahendra Ratna Campus Tahachal, Tribhuvan University, Nepal. I completed my Ph.D. in education from Banaras Hindu University, India, and my M.Ed. in mathematics education from Tribhuvan University. My research interests include educational technology, digital pedagogy, applied mathematics, quantitative research, item response theory (IRT), structural equation modeling (SEM), machine learning, and various issues related to digital pedagogy. I have been actively involved in facilitating digital pedagogy, mathematics teaching-related software and applications, and quantitative data analysis tools such as SPSS and JASP.
-
-
-
           </p>
           <p className="text-gray-300 text-center leading-relaxed mt-4">
             I also work with structural equation modeling tools like AMOS, referencing tools like Mendeley and Zotero, qualitative data analysis tools like Atlas.ti, academic/scientific writing, and other teaching-learning and research-related software and applications. Additionally, I have substantial experience in curriculum development, report writing, data analysis, thesis supervision, research project development, monitoring and evaluation of research projects, and test item writing.
